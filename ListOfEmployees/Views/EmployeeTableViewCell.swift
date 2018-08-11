b43b6ee22305bb57bdfe797e7838d8d338b15d0a
Andrius Shiaulis <shiaulis@gmail.com>
2018-08-12 00:30:26 +0300

    - Add application icon.

---
+++ ListOfEmployees/Views/EmployeeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmployeeTableViewCell.swift
+//  ListOfEmployees
+//
+//  Created by Andrius Shiaulis on 12.08.2018.
+//  Copyright © 2018 Andrius Shiaulis. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
