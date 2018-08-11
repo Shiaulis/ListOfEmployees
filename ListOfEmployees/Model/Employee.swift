cf301a104ea703b5ba212d06dc7ae504381f82e1
Andrius Shiaulis <shiaulis@gmail.com>
2018-08-12 00:04:15 +0300

    - Move employee structure in separate file.

--- ListOfEmployees/Model/Employee.swift
+++ ListOfEmployees/Model/Employee.swift
@@ -8,15 +8,61 @@
 
 import Foundation
 
-//struct Employee {
-//
-//    // MARK: - Properties -
-//
-//
-//
-//    // MARK: - Initialization -
-//
-//    init() {
-//
-//    }
-//}
+struct Employee: Decodable {
+    let firstName: String?
+    let lastName: String?
+    let position: EmployeePosition?
+    let contactDetails: ContactDetails?
+    let projects: [String]?
+
+    enum CodingKeys: String, CodingKey {
+        case firstName = "fname"
+        case lastName = "lname"
+        case position
+        case contactDetails = "contact_details"
+        case projects
+    }
+}
+
+extension Employee: Comparable {
+    static func < (lhs: Employee, rhs: Employee) -> Bool {
+
+        guard let leftLastName = lhs.lastName, let rightLastName = rhs.lastName else {
+            return lhs.lastName == nil && rhs.lastName == nil
+        }
+
+        if leftLastName != rightLastName {
+            return leftLastName < rightLastName
+        }
+
+        guard let leftFirstName = lhs.firstName, let rightFirstName = rhs.firstName else {
+            return lhs.firstName == nil && rhs.firstName == nil
+        }
+
+        return leftFirstName < rightFirstName
+    }
+}
+
+extension Employee: Equatable {
+    static func == (lhs: Employee, rhs: Employee) -> Bool {
+        if lhs.lastName != rhs.lastName {
+            return lhs.lastName == rhs.lastName
+        }
+        return lhs.firstName == rhs.firstName
+    }
+}
+
+enum EmployeePosition: String, Decodable {
+    case ios = "IOS"
+    case pm = "PM"
+    case other = "OTHER"
+    case web = "WEB"
+    case android = "ANDROID"
+    case sales = "SALES"
+    case tester = "TESTER"
+}
+
+struct ContactDetails: Decodable {
+    let email: String?
+    let phone: String?
+}
--- ListOfEmployees/Model/JsonParser.swift
+++ ListOfEmployees/Model/JsonParser.swift
@@ -32,7 +32,7 @@ class JsonParser {
             do {
                 var employees: [Employee] = []
                 for data in datas {
-                    let parsedEmployees = try strongSelf.decoder.decode(EmployeesCodable.self, from: data).employees
+                    let parsedEmployees = try strongSelf.decoder.decode(Json.self, from: data).employees
                     employees.append(contentsOf: parsedEmployees)
                 }
 
@@ -45,65 +45,8 @@ class JsonParser {
     }
 }
 
-struct EmployeesCodable: Decodable {
+struct Json: Decodable {
     let employees: [Employee]
 }
 
-struct Employee: Decodable {
-    let firstName: String?
-    let lastName: String?
-    let position: EmployeePosition?
-    let contactDetails: ContactDetails?
-    let projects: [String]?
 
-    enum CodingKeys: String, CodingKey {
-        case firstName = "fname"
-        case lastName = "lname"
-        case position
-        case contactDetails = "contact_details"
-        case projects
-    }
-}
-
-extension Employee: Comparable {
-    static func < (lhs: Employee, rhs: Employee) -> Bool {
-
-        guard let leftLastName = lhs.lastName, let rightLastName = rhs.lastName else {
-            return lhs.lastName == nil && rhs.lastName == nil
-        }
-
-        if leftLastName != rightLastName {
-            return leftLastName < rightLastName
-        }
-
-        guard let leftFirstName = lhs.firstName, let rightFirstName = rhs.firstName else {
-            return lhs.firstName == nil && rhs.firstName == nil
-        }
-
-        return leftFirstName < rightFirstName
-    }
-}
-
-extension Employee: Equatable {
-    static func == (lhs: Employee, rhs: Employee) -> Bool {
-        if lhs.lastName != rhs.lastName {
-            return lhs.lastName == rhs.lastName
-        }
-        return lhs.firstName == rhs.firstName
-    }
-}
-
-enum EmployeePosition: String, Decodable {
-    case ios = "IOS"
-    case pm = "PM"
-    case other = "OTHER"
-    case web = "WEB"
-    case android = "ANDROID"
-    case sales = "SALES"
-    case tester = "TESTER"
-}
-
-struct ContactDetails: Decodable {
-    let email: String?
-    let phone: String?
-}
