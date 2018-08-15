a1a21b131a72f12cf734aae3eaf589255eca3498
Andrius Shiaulis <shiaulis@gmail.com>
2018-08-15 07:41:59 +0300

    - Set tint color for all navigation bar buttons.

--- ListOfEmployees/Model/AppDelegate.swift
+++ ListOfEmployees/Model/AppDelegate.swift
@@ -43,6 +43,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let navigationController = UINavigationController.init(rootViewController: employeesTableViewController)
         window.rootViewController = navigationController
         window.tintColor = #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1)
+        UIBarButtonItem.appearance().tintColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
         window.makeKeyAndVisible()
         
         return true
